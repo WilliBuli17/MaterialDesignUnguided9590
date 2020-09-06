6327d050c88db14287c0579594f5b6d6342cb549
WilliBuli17 <buliwilli17@gmail.com>
2020-09-06 09:08:29 +0700

    master file

---
+++ app/src/main/java/com/ninnanovila/materialdesign/MainActivity.java
@@ -0,0 +1,82 @@
+package com.ninnanovila.materialdesign;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import com.google.android.material.button.MaterialButton;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Receipt receipt = new Receipt();
+    private List<String> checked = new ArrayList<>();
+
+    private ReceiptRecyclerViewAdapter recyclerViewAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Receipt[] receipts = receipt.getAllReceipt();
+        recyclerViewAdapter = new ReceiptRecyclerViewAdapter(receipts, this);
+
+        RecyclerView recyclerView = findViewById(R.id.recycler_receipt);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(recyclerViewAdapter);
+
+        MaterialButton checkedFloatBtn = findViewById(R.id.checked_fab);
+        checkedFloatBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO 4.1(2,5)
+                //Melakukan inisialisasi "checked" yang didapat dari fungsi yang berada pada recyclerViewAdapter
+                checked = recyclerViewAdapter.returnCheckedRecipt();
+                if (checked.isEmpty()){
+                    Toast.makeText(v.getContext(), "Just check, please!", Toast.LENGTH_SHORT).show();
+                }else {
+                    //TODO 4.4(2,5)
+                    //Fungsi menampilkan dialog dipanggil disini
+                    showSimpleDialog();
+                }
+            }
+        });
+    }
+
+    //TODO 4.2(10)
+    //Menampilkan simple dialog yang menampilkan title receipt yang checked
+    public  void showSimpleDialog(){
+        CharSequence[] charSequences = getStringArray(checked);
+        String simpleDialog = "";
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        for(int i = 0; i < checked.size(); i++) {
+            simpleDialog = simpleDialog + "\n" + charSequences[i];
+        }
+        builder.setTitle("Checked Receipt!");
+        builder.setMessage(simpleDialog);
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
+
+    //TODO 4.3(5)
+    //Apa maksud fungsi ini ? (2 kalimat)
+    //Jawaban :
+    //Mengambil nilai yang diimputkan user kedalam variabel dengan tipe data string biasa
+    //lalu memasukkannya kedalam string array.
+    public static String[] getStringArray(List<String> input) {
+        String[] strings = new String[input.size()];
+
+        for (int j = 0; j < input.size(); j++) {
+            strings[j] = input.get(j);
+        }
+
+        return strings;
+    }
+
+}
---
+++ app/src/main/java/com/ninnanovila/materialdesign/ReceiptRecyclerViewAdapter.java
@@ -0,0 +1,107 @@
+package com.ninnanovila.materialdesign;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import com.bumptech.glide.Glide;
+import com.google.android.material.card.MaterialCardView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReceiptRecyclerViewAdapter extends RecyclerView.Adapter<ReceiptRecyclerViewAdapter.ReceiptViewHolder> {
+
+    private Receipt[] receipts;
+    private Activity activity;
+    //TODO 3.1(2,5)
+    //Membuat variabel penampung untuk title receipt yang "checked"
+    List<String> checked = new ArrayList<>();
+    Receipt receipt = new Receipt();
+
+    public ReceiptRecyclerViewAdapter(Receipt[] receipts, Activity activity) {
+        this.receipts = receipts;
+        this.activity = activity;
+    }
+
+    //TODO 3.2(5)
+    //Membuat fungsi yang mengembalikan list title receipt yang "checked"
+    public  List<String> returnCheckedRecipt(){
+        return checked;
+    }
+
+    @NonNull
+    @Override
+    public ReceiptViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_receipt, parent, false);
+        return new ReceiptViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final ReceiptViewHolder holder, final int position) {
+        final Receipt receipt = receipts[position];
+        holder.nameReceipt.setText(receipt.getTitle());
+        holder.ingredientsReceipt.setText(receipt.getIngredients());
+        if (!receipt.getImageUrl().equals("")){
+            Glide.with(activity)
+                    .load(receipt.getImageUrl())
+                    .centerCrop()
+                    .into(holder.imageReceipt);
+        }else{
+            holder.imageReceipt.setImageResource(R.drawable.ic_broken_image);
+        }
+
+        holder.itemCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                holder.itemCard.setChecked(!holder.itemCard.isChecked());
+                //TODO 3.4(2,5)
+                //Fungsi untuk menambah ataupun menghapus dipanggil disini
+                addAndRemove(holder, v, holder.getAdapterPosition());
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return receipts.length;
+    }
+
+    //TODO 3.3(10)
+    //Membuat fungsi untuk menambahkan title receipt yang checked dan menghapus yang unchecked
+    public void addAndRemove(ReceiptViewHolder holder, View view, int x){
+        if(holder.itemCard.isChecked()){
+            checked.add(receipts[x].getTitle());
+            Toast.makeText(view.getContext(),
+                    "Input : " + receipts[x].getTitle(),
+                    Toast.LENGTH_SHORT).show();
+        } else{
+          checked.remove(receipts[x].getTitle());
+          Toast.makeText(view.getContext(),
+                  "Delete : " + receipts[x].getTitle(),
+                  Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    static class ReceiptViewHolder extends RecyclerView.ViewHolder {
+
+        private ImageView imageReceipt;
+        private TextView nameReceipt;
+        private TextView ingredientsReceipt;
+        private MaterialCardView itemCard;
+
+        public ReceiptViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageReceipt = itemView.findViewById(R.id.image_receipt);
+            nameReceipt = itemView.findViewById(R.id.name_receipt);
+            ingredientsReceipt = itemView.findViewById(R.id.ingredients_receipt);
+            itemCard = itemView.findViewById(R.id.item_card);
+        }
+    }
+}
